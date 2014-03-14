d31d9581a55f776346719703538e7d03064f8c42
laurakwiley <laura.k.wiley@chgr.mc.vanderbilt.edu>
2014-03-14 11:18:17 -0500

    Update extract_statin_allergies_problem_list.R

--- extract_statin_allergies_problem_list.R
+++ extract_statin_allergies_problem_list.R
@@ -5,9 +5,9 @@
 ## March 13, 2014
 ## R v3.0.2
 ##
-## extract_statin_myopathy_original_list() - keywords from training set of 300 records from Vanderbilt's deidentified electronic medical record database. These records were not reviewed manually to make an absolute determination of case/control status.
+## extract_statin_myopathy_pl_allergy_original_list() - keywords from training set of 300 records from Vanderbilt's deidentified electronic medical record database. These records were not reviewed manually to make an absolute determination of case/control status.
 ##
-## extract_statin_myopathy_corected_list() - keywords contain both those in the original list, and those that were found in another 300 records from the same database. This set of records had their entire record reviewed and contained a total of 124 confirmed stain myotoxicity cases and 176 controls.
+## extract_statin_myopathy_pl_allergy_corected_list() - keywords contain both those in the original list, and those that were found in another 300 records from the same database. This set of records had their entire record reviewed and contained a total of 124 confirmed stain myotoxicity cases and 176 controls.
 ##
 ## Each function: 
 ##		1. first extracts statins from the allergy list
@@ -19,7 +19,7 @@
 ############################################################
 
 
-extract_statin_myopathy_original_list<-function(x){
+extract_statin_myopathy_pl_allergy_original_list<-function(x){
   statin_list<-c("atorvastin","fluvastatin","lovastatin","pitavastatin","pravastatin","rosuvastatin","simvastatin"," statin ","statins","hmg","lipitor","caduet","lescol","mevacor","altocor","altoprev","livalo","pitava","pravachol","crestor","zocor","vytorin","simcor")
   
   not_myopathy_keywords<-c("lft","liver","swelling","hives","headache","night trerrors","night terrors","nausea","vomiting","mg ","rash","pruritis","gi ","current medications","constipation","renal","on.*therapy","mg\\.")
@@ -32,7 +32,7 @@ extract_statin_myopathy_original_list<-function(x){
   return(statin_myopathy)
 }
 
-extract_statin_myopathy_corected_list<-function(x){
+extract_statin_myopathy_pl_allergy_corected_list<-function(x){
   statin_list<-c("atorvastin","fluvastatin","lovastatin","pitavastatin","pravastatin","rosuvastatin","simvastatin"," statin ","statins","hmg","lipitor","caduet","lescol","mevacor","altocor","altoprev","livalo","pitava","pravachol","crestor","zocor","vytorin","simcor")
 
   not_myopathy_keywords_testset<-c("diarrhea","nausea","black spots","lft","breath","liver","gi ","headache","dizz","cough","rash","edema","swollen","ha","h/a","qhs","every night","bedtime","hallucinations","proctitis","flatulence","stool","nosebleeds","swelling","hives","pancreatitis","constipation","skin crawls","sore throat","eczema","sweaty","stomach","flushed","urinary","heat","transaminases","qd","hepatitis","urination","gastrointestinal","light headed","increased bp","night terrors","daily","hepatic","hepatotoxicity","anaphylaxis","n/v","every day","increased ed","renal","blurred vision","hyperkalemia","anxiety","itching","night trerrors","bid","sob","night terrors","vomiting"," mg ","[0-9]+mg","pruritis","gi ","current medications","on.*therapy","mg\\.")
